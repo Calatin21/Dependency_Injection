eb806ec7b038fc7ca57c0bc808b13d3d28b81d02
Calatin21 <oshaughnessy@hotmail.de>
2023-05-10 10:40:31 +0200

    patch

--- Dependency_Injection/Lehrling.cs
+++ Dependency_Injection/Lehrling.cs
@@ -1,6 +1,11 @@
 ﻿namespace Dependency_Injection {
     internal class Lehrling {
         public void GrabeLoch(IGrabable grabwerkzeug) {
+            if (grabwerkzeug is Bagger) {
+                Console.WriteLine("Lehrling sagt: Nice mit dem bagger wollte ich schon immer baggern!");
+            } else if (grabwerkzeug is Loeffel) {
+                Console.WriteLine("Lehrling sagt: Nein, mit nem Löffel, Meister was soll dass denn?");
+            }
             grabwerkzeug.Buddel();
         }
     }
